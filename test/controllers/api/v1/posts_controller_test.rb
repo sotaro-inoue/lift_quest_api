ef632de2598ef3d5403a965f5e61328a4ac81a53
sotaro-inoue <ino.s0212@gmail.com>
2024-12-21 01:06:28 +0900

    トレーニング記録周りのMCを実装しました

--- test/controllers/api/v1/posts_controller_test.rb
+++ test/controllers/api/v1/posts_controller_test.rb
@@ -1,28 +1,32 @@
 require "test_helper"
 
 class Api::V1::PostsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @post = posts(:one)
+  end
+
   test "should get index" do
-    get api_v1_posts_index_url
+    get api_v1_posts_url
     assert_response :success
   end
 
   test "should get show" do
-    get api_v1_posts_show_url
+    get api_v1_post_url(@post)
     assert_response :success
   end
 
-  test "should get create" do
-    get api_v1_posts_create_url
+  test "should create post" do
+    post api_v1_posts_url, params: { post: {} }
     assert_response :success
   end
 
-  test "should get update" do
-    get api_v1_posts_update_url
+  test "should update post" do
+    patch api_v1_post_url(@post), params: { post: {} }
     assert_response :success
   end
 
-  test "should get destroy" do
-    get api_v1_posts_destroy_url
+  test "should destroy post" do
+    delete api_v1_post_url(@post)
     assert_response :success
   end
 end
--- test/controllers/api/v1/workout_logs_controller_test.rb
+++ test/controllers/api/v1/workout_logs_controller_test.rb
@@ -1,28 +1,32 @@
 require "test_helper"
 
 class Api::V1::WorkoutLogsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @workout_log = workout_logs(:one)
+  end
+
   test "should get index" do
-    get api_v1_workout_logs_index_url
+    get api_v1_workout_logs_url
     assert_response :success
   end
 
   test "should get show" do
-    get api_v1_workout_logs_show_url
+    get api_v1_workout_log_url(@workout_log)
     assert_response :success
   end
 
-  test "should get create" do
-    get api_v1_workout_logs_create_url
+  test "should create workout_log" do
+    post api_v1_workout_logs_url, params: { workout_log: {} }
     assert_response :success
   end
 
-  test "should get update" do
-    get api_v1_workout_logs_update_url
+  test "should update workout_log" do
+    patch api_v1_workout_log_url(@workout_log), params: { workout_log: {} }
     assert_response :success
   end
 
-  test "should get destroy" do
-    get api_v1_workout_logs_destroy_url
+  test "should destroy workout_log" do
+    delete api_v1_workout_log_url(@workout_log)
     assert_response :success
   end
 end
